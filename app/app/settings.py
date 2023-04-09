a978eb1aef48c9eea6f115b5db94735446810f81
Sherif <sherifkunch@gmail.com>
2023-04-09 20:18:04 +0300

    da

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'CHooseYourProductionSecretKey'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['django-aks-ingress.westeurope.cloudapp.azure.com']
 
 # Application definition
 
